254504793d089b39669d612f566686aacd3ea6b8
working-zima <working.zima@gmail.com>
2023-08-05 20:10:45 +0900

    chore: 빌드 준비

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ import ErrorPage from "./pages/Error";
 import TodoPage from "./pages/TodoPage";
 import { privateLoader, publicLoader, firstPageLoader } from "./utils/auth";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    id: "root",
-    children: [
-      { index: true, element: <HomePage />, loader: firstPageLoader },
-      { path: "signup", element: <SignUpPage />, loader: privateLoader },
-      { path: "signin", element: <SignInPage />, loader: privateLoader },
-      { path: "todo", element: <TodoPage />, loader: publicLoader },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      id: "root",
+      children: [
+        { index: true, element: <HomePage />, loader: firstPageLoader },
+        { path: "signup", element: <SignUpPage />, loader: privateLoader },
+        { path: "signin", element: <SignInPage />, loader: privateLoader },
+        { path: "todo", element: <TodoPage />, loader: publicLoader },
+      ],
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 function App() {
   return <RouterProvider router={router} />;
